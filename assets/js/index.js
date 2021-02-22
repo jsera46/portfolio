f2b293c13244635efc4ad9c365bfff56cbc054b5
jsera46 <jsera46@gmail.com>
2021-02-22 16:52:24 +0800

    update iphone design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,19 @@
-index.js
+$(document).ready(function(){
+    let buttonClick = false;
+    $('#burger').click(function(){
+        if (buttonClick == false){
+            $('#bars').removeClass('bar');
+            $('#bars').addClass('bar1');
+            $('#navMC').addClass('animate__fadeIn');
+            $('#navMC').toggleClass('open');;
+            $('.menu-content').css('display', 'flex');
+            buttonClick = true;
+        }else{
+            $('#navMC').removeClass('animate__fadeIn');
+            $('#bars').removeClass('bar1');
+            $('#bars').addClass('bar');
+            $('#navMC').css('display', 'none');
+            buttonClick = false;            
+        }
+    });
+});

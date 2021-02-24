16cbcbb6b18e718fc18b2e669a6ac8e5febbcbd9
jsera46 <jsera46@gmail.com>
2021-02-24 16:48:50 +0800

    update desktop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+    // Nav-menu
+    $('.deskHome').click(function(){
+        $('.deskHome').addClass('underline');
+        $('.deskProjects').removeClass('underline');
+        $('.deskAbout').removeClass('underline');
+    })
+
+    $('.deskProjects').click(function(){
+        $('.deskHome').removeClass('underline');
+        $('.deskProjects').addClass('underline');
+        $('.deskAbout').removeClass('underline');
+    })
+
+    $('.deskAbout').click(function(){
+        $('.deskHome').removeClass('underline');
+        $('.deskProjects').removeClass('underline');
+        $('.deskAbout').addClass('underline');
+    })
+    
+    // hamburger
     let buttonClick = false;
     $('#burger').click(function(){
         if (buttonClick == false){

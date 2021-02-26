47a4ce2d04b99ea30dd4d475e38f42df620dfaf2
jsera46 <jsera46@gmail.com>
2021-02-26 16:30:04 +0800

    2231

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,26 +3,41 @@ $(document).ready(function(){
     window.onscroll = function(){
       scrollFunction()
     };
-    
+    let home = $('#home');
+    let projects = $('#projects');
+    let about = $('#about');
+    let contact = $('#contact-btn');
+    let navbar = $('#navbar');
+    let navMC = $('#navMC');
+    let navsize = $('.deskHome, .deskProjects, .deskAbout, .navButton')
+    let logo = $('.logo');
+
     function scrollFunction(){
       if (document.body.scrollTop > 1 || document.documentElement.scrollTop >1) {
         $('#logo1').css('font-size', '1.3rem');
         $('#navbar').css({
-          'backdrop-filter': 'blur(50px)',
+          'background-color': '#273037',
           'border-bottom': '1px solid rgba(153, 151, 151, .3',
           'padding': '1rem 0'
         });
-        // document.getElementById("hamburger-menu").style.height = "0.5rem";
+        $('.mycontainerDesk').css({'background-color': '#273037'});
+        $('.menu-content').css('top', '6px');
         
+        navsize.css('font-size', '1rem');
+        logo.css('font-size', '1.3rem');
       } else {
         $('#logo1').css('font-size', '1.5rem');
         $('#navbar').css({
-          'backdrop-filter': 'none',
+          'background-color': 'transparent',
           'border-bottom': 'none',
           'padding': '2rem 0'
         });
-        // document.getElementById("navbar").style.padding = "2rem 0";
-        // document.getElementId('logo1').style.font-size = "1.5rem";
+        $('.mycontainerDesk').css({
+          'background-color': 'transparent'
+        });
+        $('.menu-content').css('top', '24px');
+        navsize.css('font-size', '1.2rem');
+        logo.css('font-size', '1.5rem');
       }
     };
     
@@ -51,21 +66,66 @@ $(document).ready(function(){
         if (buttonClick == false){
             $('#bars').removeClass('bar');
             $('#bars').addClass('bar1');
-            $('#navMC').addClass('animate__fadeIn');
-            $('#navMC').toggleClass('open');;
+            navMC.fadeIn(400);
+            // $('#navMC').toggleClass('open');
             $('.menu-content').css('display', 'flex');
             buttonClick = true;
-            $('#navbar').css('backdrop-filter', 'none');            
-            $('#bars').css('margin-top', '12px');
         }else{
-            $('#navMC').removeClass('animate__fadeIn');
+            $('#navMC').removeClass('animate__fadeIn animate__animated animate__faster');
             $('#bars').removeClass('bar1');
             $('#bars').addClass('bar');
             // $('#navMC').css('display', 'none');
-            $('#navMC').hide();
+            $('#navMC').fadeOut(800);
             
-            $('#bars').css('margin-top', '0');
+            // $('#bars').css('margin-top', '0');
             buttonClick = false;
         }
     });
+    
+    // iphone navMC links
+    home.click(function(){      
+      navMC.delay(800).fadeOut(400);
+      setTimeout(function(){
+        $('#bars').removeClass('bar1');
+      }, 800);
+      setTimeout(function(){
+        $('#bars').addClass('bar');
+      }, 800);
+    })
+
+    projects.click(function(){
+      navMC.delay(800).fadeOut(400);
+      setTimeout(function(){
+        $('#bars').removeClass('bar1');
+      }, 800);
+      setTimeout(function(){
+        $('#bars').addClass('bar');
+      }, 800);
+    })
+
+    about.click(function(){
+      navMC.delay(800).fadeOut(400);
+      setTimeout(function(){
+        $('#bars').removeClass('bar1');
+      }, 800);
+      setTimeout(function(){
+        $('#bars').addClass('bar');
+      }, 800);
+    })
+    
+    contact.click(function(){
+      navMC.delay(800).fadeOut(400);
+      setTimeout(function(){
+        $('#bars').removeClass('bar1');
+      }, 800);
+      setTimeout(function(){
+        $('#bars').addClass('bar');
+      }, 800);
+    });
+
+    // Scrollspy Navbar
+    
+
+
+
 });
